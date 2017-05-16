38bc3f8dbc715936eb06f27827d0e1566cb6ad96
Yi Li <liyi64125837@163.com>
2017-05-15 20:11:32 -0400

    added LC448

--- Sum1/Disappeared_Nums.java
+++ Sum1/Disappeared_Nums.java
@@ -1,5 +1,19 @@
 package Sum1;
 
-public class Disappeared_Nums {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Disappeared_Nums {
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+        if(nums == null || nums.length < 1) return new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i ++){
+        		int index = Math.abs(nums[i]) - 1;
+        		if(nums[index] > 0) nums[index] = -nums[index];
+        }
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i ++){
+        		if(nums[i] > 0) res.add(i + 1);
+        }
+        return res;
+    }
 }
