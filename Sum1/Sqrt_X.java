c6ffc7f97c62e9f694a53501d80fc354a0be24d8
Yi Li <liyi64125837@163.com>
2017-06-19 18:56:01 -0400

    added LC69

--- Sum1/Sqrt_X.java
+++ Sum1/Sqrt_X.java
@@ -3,7 +3,7 @@ package Sum1;
 public class Sqrt_X {
 	public int mySqrt(int x) {
         if(x == 1 || x == 0) return x;
-        long low = 1, hi = x;
+        long low = 1, hi = x/2;
         while(low <= hi){
         		long mid = (hi - low) / 2 + low;
         		if(mid * mid <= x && (mid + 1) * (mid + 1) > x) return (int)mid;
