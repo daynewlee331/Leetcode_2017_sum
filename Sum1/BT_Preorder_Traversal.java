a44520afbd1892069c72e0984f840307e9980dbd
Yi Li <liyi64125837@163.com>
2017-06-22 21:35:41 -0400

    added LC144

---
+++ Sum1/BT_Preorder_Traversal.java
@@ -0,0 +1,21 @@
+package Sum1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BT_Preorder_Traversal {
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<>();
+		if(root == null) return res;
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+		while(!stack.isEmpty()){
+			root = stack.pop();
+			res.add(root.val);
+			if(root.right != null) stack.push(root.right);
+			if(root.left != null) stack.push(root.left);
+		}
+		return res;
+    }
+}
